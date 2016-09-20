eed06d2d9e6407ea55e01d1a7d3a0ede7801e3b6
Isaac Bowes <isaac.bowes@ad.aie.edu>
2016-09-20 12:15:52 +0930

    Added in Raycast Script

--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -4,24 +4,31 @@ using System.Collections;
 public class Raycast : MonoBehaviour
 {
 
-	// Use this for initialization
-	void Start ()
-    {
-	
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    void RayCast()
     {
         RaycastHit hit;
-        Ray landingRay = new Ray(transform.position, Vector3.forward);
+        float rayDistance;
 
-        if (Physics.Raycast(landingRay, out hit))
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(transform.position, forward, Color.green);
+
+        if (Physics.Raycast(transform.position, (forward), out hit))
         {
-            if (hit.collider.tag == "Enemy")
-            {
-
-            }
+            rayDistance = hit.distance;
+            print(rayDistance + " " + hit.collider.gameObject.name);
         }
-	}
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        RayCast();
+    }
 }
