21b72425bbf9fdf3123fc54ac06a98009e7c4dc4
AIE\bradley.court <bradleycourt@hotmail.com>
2016-09-26 15:49:11 +0930

    Addded flash, code needs fixing on the health script, forgot to delete arbitrary script named damage flash (fuck)

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour 
 {
-	public int health = 100;
-	public int currentHealth;
+	public int health = 5;
+    public int lastHealth;
+    public int hit; // (being damaged)
+	//public int currentHealth;
 	public Image damageImage;  
 	public float flashSpeed = 5f;
 	bool isDead;
@@ -12,26 +15,58 @@ public class Health : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
-		currentHealth = health;
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		// If the player has just been damaged...
-		if(damaged)
-		{
-			// ... set the colour of the damageImage to the flash colour.
-			damageImage.color = flashColour;
-		}
-		// Otherwise...
-		else
-		{
-			// ... transition the colour back to clear.
-			damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
-		}
 
-		// Reset the damaged flag.
-		damaged = false;
 	}
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        if (health > 5)
+        {
+            health = 5;
+        }
+        if (health <= 0)
+        {
+            health = 0;
+        }
+
+        if ()//Health starts at 5, After damage is 4
+        //set lastHealth to health, stop screenflash
+        )
+
+        {
+
+
+            if (damaged == true)
+            {
+
+                damaged = true;
+                Color col = damageImage.color;
+                col.a = 1;
+                damageImage.color = col;
+
+                health--;
+                // ... set the colour of the damageImage to the flash colour.
+                //damageImage.color = flashColour;
+            }
+            // Otherwise...
+            else if (damaged = true && health <= 0)
+            {
+                isDead = true;
+                //Works, proven by the Debug code.
+                //Debug.Break();
+                // ... transition the colour back to clear.
+                //damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+            }
+
+            damaged = false;
+            
+        }
+        lastHealth = health;
+    }
+
+        
 }
+
