f6419af5f6e15e419e4ed645262a306827998de4
AIE\bradley.court <bradleycourt@hotmail.com>
2016-10-24 15:24:03 +1030

    Ai is not mentally unstable

--- Assets/Scripts/Enemy/FollowPlayer.cs
+++ Assets/Scripts/Enemy/FollowPlayer.cs
@@ -1,18 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FollowPlayer : MonoBehaviour {
+public class FollowPlayer : MonoBehaviour
+{
     private NavMeshAgent agent;
     public GameObject player;
-
+    public float attackRange = 0f;
+    float Distance;
+    public float Defaultspeed = 3.5f;
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
     }
     void Update()
     {
         //GetComponent<NavMeshAgent>().destination = player.transform.position;
-        agent.SetDestination(player.transform.position);
-        
+        attack();
+        Distance = Vector3.Distance(transform.position, player.transform.position);
+
+
+    }
+    public void attack()
+    { 
+      
+        if (Distance <= attackRange)
+        {
+         
+            agent.speed = 0;
+        }
+        else
+        {
+            agent.speed = Defaultspeed;
+            agent.SetDestination(player.transform.position);
+        }
     }
 }
