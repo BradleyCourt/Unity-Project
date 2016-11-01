03fc1efcdb67dc97820280943dda3741ec8563a0
AIE\connor.molseed <toothbrush112233@gmail.com>
2016-11-01 12:29:11 +1030

    Fixed TableEditor style error, add WaveController, and Enemy Spawner Prefab.

--- Assets/Scripts/GameControl/WaveController.cs
+++ Assets/Scripts/GameControl/WaveController.cs
@@ -6,13 +6,58 @@ public class WaveController : MonoBehaviour
     public Transform[] spawnPoints;
     public int remainingSpawns;
     public int remainingEnemies;
+    public int waveNumber;
+    public float spawnPeriod;
     float enemiesPerSecond;
+    public int baseEnemiesPerWave;
+    public int enemyIncreasePerWave;
     static WaveController instance;
 
 
 	void Start ()
     {
-        //if (instance == null )
+        if (instance == null || instance == this)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+        baseEnemiesPerWave -= enemyIncreasePerWave;
+        StartCoroutine(SpawnWave());
 	}
-	
+
+    IEnumerator SpawnWave()
+    {
+        remainingEnemies = remainingSpawns = CalcEnemiesPerWave();
+        enemiesPerSecond = CalcEnemiesPerSecond(remainingSpawns);
+        print("EPS: " + enemiesPerSecond);
+        int enemiesPerPulse;
+        while (remainingSpawns > 0)
+        {
+            enemiesPerPulse = (remainingSpawns < spawnPoints.Length) ? remainingSpawns : spawnPoints.Length;
+            remainingSpawns -= enemiesPerPulse;
+            for (int i = enemiesPerPulse; i > 0; i -= 1)
+            {
+                print(spawnPoints[i - 1].position.ToString());
+            }
+            yield return new WaitForSeconds(enemiesPerSecond);
+        }
+    }
+
+    float CalcEnemiesPerSecond(int totalEnemies)
+    {
+        if (spawnPoints.Length == 0) { return 0; }
+
+        float enemiesPerSecond = ((totalEnemies / spawnPeriod) / spawnPoints.Length);
+        return enemiesPerSecond;
+    }
+
+    int CalcEnemiesPerWave()
+    {
+        int enemiesPerWave = baseEnemiesPerWave + (enemyIncreasePerWave * waveNumber);
+        return enemiesPerWave;
+    }
 }
