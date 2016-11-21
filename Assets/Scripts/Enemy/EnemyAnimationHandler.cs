4fed512fdfb5c93cada0a29b6805606576ca63d6
AIE\connor.molseed <toothbrush112233@gmail.com>
2016-11-21 15:31:15 +1030

    Added enemy model and animation system

---
+++ Assets/Scripts/Enemy/EnemyAnimationHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAnimationHandler : MonoBehaviour
+{
+    public Animator animCont;
+    public EnemyHealth enemyHP;
+    public NavMeshAgent navCont;
+
+	void Update ()
+    {
+        float speed = (new Vector2 (navCont.velocity.x, navCont.velocity.z)).magnitude;
+        animCont.SetFloat("movementSpeed", speed);
+	}
+}
